21a8f41424a239bbf57b1df63ac2f84ad86575aa
tvbegovic <prof@KAT42>
2023-12-18 11:04:46 +0100

    ostale get funkcije, search, static files

--- GameshopWeb/Controllers/GameController.cs
+++ GameshopWeb/Controllers/GameController.cs
@@ -33,5 +33,40 @@ namespace GameshopWeb.Controllers
                 return db.Query<Company>("SELECT * FROM Company ORDER BY Name").ToList();
             }
         }
+
+        [HttpGet("bygenre/{id}")]
+        public List<Game> GetByGenre(int id)
+        {
+            using (var db = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return db.Query<Game>("SELECT * FROM Game WHERE idGenre = @id", new { id }).ToList();
+            }
+        }
+
+        [HttpGet("bycompany/{id}")]
+        public List<Game> GetByCompany(int id)
+        {
+            using (var db = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return db.Query<Game>("SELECT * FROM Game WHERE idPublisher = @id OR idDeveloper = @id",
+                    new { id }).ToList();
+            }
+        }
+
+        [HttpGet("search/{text}")]
+        public List<Game> Search(string text)
+        {
+            using (var db = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return db.Query<Game>(
+                    @"SELECT Game.* FROM Game
+                    INNER JOIN Genre ON Game.idGenre = Genre.id
+                    INNER JOIN Company Publisher ON Game.idPublisher = Publisher.id
+                    INNER JOIN Company Developer ON Game.idDeveloper = Developer.id
+                    WHERE Game.title LIKE @text OR Genre.name LIKE @text
+                    OR Publisher.name LIKE @text OR Developer.name LIKE @text",
+                    new { text = $"%{text}%" }).ToList();
+            }
+        }
     }
 }
--- GameshopWeb/Program.cs
+++ GameshopWeb/Program.cs
@@ -21,5 +21,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.Run();
